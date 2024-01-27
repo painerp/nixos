337439a2bf7cd699227be089e4fc7631b811fd07
painerp <8081128+painerp@users.noreply.github.com>
2024-01-27 10:32:14 +0100

    restructure secrets and add new secrets for multiple hosts

--- secrets/default.nix
+++ secrets/default.nix
@@ -2,23 +2,40 @@
   tailscale-api-key = ./tailscale-api-key.age;
 
   # containers
-  teamspeak-env = ./containers/teamspeak.env.age;
   traefik-env = ./containers/traefik.env.age;
   watchtower-env = ./containers/watchtower.env.age;
-  authentik-env = ./containers/authentik.env.age;
-  authentik-pg-env = ./containers/authentik-pg.env.age;
-  bachelor-env = ./containers/bachelor.env.age;
-  bachelor-pg-env = ./containers/bachelor-pg.env.age;
-  nuxt-pages-env = ./containers/nuxt-pages.env.age;
-  nuxt-pages-mysql-env = ./containers/nuxt-pages-mysql.env.age;
-  nuxt-pages-pma-env = ./containers/nuxt-pages-pma.env.age;
-  nuxt-pages-g2g-env = ./containers/nuxt-pages-g2g.env.age;
 
   # variants
-  bpi-user-pw = ./bpi/user-pw.age;
 	bpi-wifi = ./bpi/wifi.age;
-  jpi-user-pw = ./jpi/user-pw.age;
+
 	jpi-wifi = ./jpi/wifi.age;
-  ext-user-pw = ./ext/user-pw.age;
-  ext-root-pw = ./ext/root-pw.age;
+
+  ext-authentik-env = ./ext/authentik.env.age;
+  ext-authentik-pg-env = ./ext/authentik-pg.env.age;
+  ext-bachelor-env = ./ext/bachelor.env.age;
+  ext-bachelor-pg-env = ./ext/bachelor-pg.env.age;
+  ext-nuxt-pages-env = ./ext/nuxt-pages.env.age;
+  ext-nuxt-pages-mysql-env = ./ext/nuxt-pages-mysql.env.age;
+  ext-nuxt-pages-pma-env = ./ext/nuxt-pages-pma.env.age;
+  ext-nuxt-pages-g2g-env = ./ext/nuxt-pages-g2g.env.age;
+  ext-teamspeak-env = ./ext/teamspeak.env.age;
+
+  inf-authentik-proxy-env = ./inf/authentik-proxy.env.age;
+  inf-jellystat-env = ./inf/jellystat.env.age;
+  inf-jellystat-pg-env = ./inf/jellystat-pg.env.age;
+
+  gra-authentik-proxy-env = ./gra/authentik.env.age;
+
+  cit-git-pw = ./cit/git-pw.age;
+  cit-authentik-env = ./cit/authentik.env.age;
+  cit-authentik-pg-env = ./cit/authentik-pg.env.age;
+
+  arr-authentik-proxy-env = ./arr/authentik-proxy.env.age;
+  arr-gluetun-env = ./arr/gluetun.env.age;
+  arr-prdl-env = ./arr/prdl.env.age;
+
+  run-act-runner-env = ./run/act-runner.env.age;
+
+  log-authentik-proxy-env = ./log/authentik-proxy.env.age;
+  log-grafana-env = ./log/grafana.env.age;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,13 @@ let
   jpi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPwO0rR4DGyCTUTBQ8eZD57Sps/AeIYTooSFKollMAV";
   bpi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsEvJHm2Nyaps1z7Pk7tUAqNd6HZLhGeV4P8JVPbkla";
   ext = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAK9i6Ywzx4IFy5I7N4/OQJfd36cShHtWa9N+7tkEn3I";
-  systems = [ jpi bpi ext ];
+  log = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINeMjHcHigAw7K5OAemN4vu3vFEwKfHZ5HCVXfpSmKbk";
+  run = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWdp6FGFKF8IlQAOf7U4gUVgFuUbSyXxjIDCcNd0Ffa";
+  inf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINeMjHcHigAw7K5OAemN4vu3vFEwKfHZ5HCVXfpSmKbk";
+  gra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINeMjHcHigAw7K5OAemN4vu3vFEwKfHZ5HCVXfpSmKbk";
+  cit = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINeMjHcHigAw7K5OAemN4vu3vFEwKfHZ5HCVXfpSmKbk";
+  arr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINeMjHcHigAw7K5OAemN4vu3vFEwKfHZ5HCVXfpSmKbk";
+  systems = [ jpi bpi ext log run inf cit arr ];
 in
 {
   # general
@@ -14,22 +20,37 @@ in
   # containers
   "containers/traefik.env.age".publicKeys = users ++ systems;
   "containers/watchtower.env.age".publicKeys = users ++ systems;
-  "containers/teamspeak.env.age".publicKeys = users ++ [ ext ];
-  "containers/authentik.env.age".publicKeys = users ++ [ ext ];
-  "containers/authentik-pg.env.age".publicKeys = users ++ [ ext ];
-  "containers/bachelor.env.age".publicKeys = users ++ [ ext ];
-  "containers/bachelor-pg.env.age".publicKeys = users ++ [ ext ];
-  "containers/nuxt-pages.env.age".publicKeys = users ++ [ ext ];
-  "containers/nuxt-pages-pma.env.age".publicKeys = users ++ [ ext ];
-  "containers/nuxt-pages-mysql.env.age".publicKeys = users ++ [ ext ];
-  "containers/nuxt-pages-g2g.env.age".publicKeys = users ++ [ ext ];
 
-  "jpi/user-pw.age".publicKeys = users ++ [ jpi ];
   "jpi/wifi.age".publicKeys = users ++ [ jpi ];
 
-  "bpi/user-pw.age".publicKeys = users ++ [ bpi ];
   "bpi/wifi.age".publicKeys = users ++ [ bpi ];
 
-  "ext/user-pw.age".publicKeys = users ++ [ ext ];
-  "ext/root-pw.age".publicKeys = users ++ [ ext ];
+  "ext/authentik.env.age".publicKeys = users ++ [ ext ];
+  "ext/authentik-pg.env.age".publicKeys = users ++ [ ext ];
+  "ext/bachelor.env.age".publicKeys = users ++ [ ext ];
+  "ext/bachelor-pg.env.age".publicKeys = users ++ [ ext ];
+  "ext/nuxt-pages.env.age".publicKeys = users ++ [ ext ];
+  "ext/nuxt-pages-pma.env.age".publicKeys = users ++ [ ext ];
+  "ext/nuxt-pages-mysql.env.age".publicKeys = users ++ [ ext ];
+  "ext/nuxt-pages-g2g.env.age".publicKeys = users ++ [ ext ];
+  "ext/teamspeak.env.age".publicKeys = users ++ [ ext ];
+
+  "inf/authentik-proxy.env.age".publicKeys = users ++ [ inf ];
+  "inf/jellystat.env.age".publicKeys = users ++ [ inf ];
+  "inf/jellystat-pg.env.age".publicKeys = users ++ [ inf ];
+
+  "cit/git-pw.age".publicKeys = users ++ [ cit ];
+  "cit/authentik.env.age".publicKeys = users ++ [ cit ];
+  "cit/authentik-pg.env.age".publicKeys = users ++ [ cit ];
+
+  "gra/authentik-proxy.env.age".publicKeys = users ++ [ gra ];
+
+  "arr/authentik-proxy.env.age".publicKeys = users ++ [ arr ];
+  "arr/gluetun.env.age".publicKeys = users ++ [ arr ];
+  "arr/prdl.env.age".publicKeys = users ++ [ arr ];
+
+  "run/act-runner.env.age".publicKeys = users ++ [ run ];
+
+  "log/authentik-proxy.env.age".publicKeys = users ++ [ log ];
+  "log/grafana.env.age".publicKeys = users ++ [ log ];
 }
