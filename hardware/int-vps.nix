7c6e4ba3c8d41d6c0c4b78cdac77bf82bffc2b08
painerp <8081128+painerp@users.noreply.github.com>
2024-01-27 10:59:30 +0100

    add run as a nixos system and add int-vps hardware config

---
+++ hardware/int-vps.nix
@@ -0,0 +1,13 @@
+{ modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+}
