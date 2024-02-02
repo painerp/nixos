b07ecdd1a68c475f2cd61cafd98027fec0ad33dc
painerp <8081128+painerp@users.noreply.github.com>
2024-02-02 02:44:25 +0100

    update gra system

--- variants/gra.nix
+++ variants/gra.nix
@@ -22,6 +22,12 @@ in {
   swapDevices =
     [{ device = "/dev/disk/by-uuid/4a6b0055-10ec-45b7-b07e-3cfcb2a57915"; }];
 
+  fileSystems."/mnt/nextcloud" = {
+    device = "10.0.10.1:/mnt/main/nextcloud";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" ];
+  };
+
   fileSystems."/mnt/motion" = {
     device = "10.0.10.1:/mnt/main/motion";
     fsType = "nfs";
@@ -40,8 +46,12 @@ in {
     };
     nextcloud = {
       enable = true;
-      proxy = true;
-      env-file = secrets.gra-authentik-proxy-env;
+      path = "/mnt/nextcloud";
+    };
+    jellyfin = {
+      enable = true;
+      internal = true;
+      path = "/mnt/motion";
     };
     monitoring = {
       node-exporter.enable = true;
