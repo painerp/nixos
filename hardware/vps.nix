aef0a98b944d10fbf67f018d85d3369f67220def
painerp <8081128+painerp@users.noreply.github.com>
2024-02-17 04:26:49 +0100

    add unified cgroups hierarchy to hardware vps conf

--- hardware/vps.nix
+++ hardware/vps.nix
@@ -5,7 +5,11 @@
 
   boot.initrd.availableKernelModules =
     [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.kernelModules =
+    [ "cgroup_enable=memory" "cgroup_enable=cpuset" "cgroup_memory=1" ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  systemd.enableUnifiedCgroupHierarchy = false;
 }
