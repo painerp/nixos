64f9ae13eaa1423677c82899cfc7a3f33c81882d
painerp <8081128+painerp@users.noreply.github.com>
2024-02-19 03:58:33 +0100

    add module options and auto import all modules by default

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,7 +1,15 @@
-{ lib, pkgs, config, secrets, ... }:
+{ lib, pkgs, config, ... }:
 
-{
-  config = {
+let cfg = config.modules.tailscale;
+in {
+  options.modules.tailscale = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+
+  config = lib.mkIf (cfg.enable) {
     environment.systemPackages = [ pkgs.tailscale ];
 
     services.tailscale.enable = true;
