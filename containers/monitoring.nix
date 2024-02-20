489258571584ca01f4ab02c2a67803192715fa3d
painerp <8081128+painerp@users.noreply.github.com>
2024-02-20 16:16:27 +0100

    move grafana ini to mounted path

--- containers/monitoring.nix
+++ containers/monitoring.nix
@@ -138,6 +138,7 @@ in {
               GF_SERVER_DOMAIN = config.server.domain;
               GF_SERVER_ROOT_URL =
                 "https://${cfg.grafana.subdomain}.${config.server.domain}";
+              GF_PATHS_CONFIG = "/var/lib/grafana/grafana.ini";
             };
             env_file = [ config.age.secrets.grafana-env.path ];
             labels = config.lib.server.mkTraefikLabels {
