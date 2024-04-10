ff3374ad8d58cba4315118ded9d122970df460f9
painerp <8081128+painerp@users.noreply.github.com>
2024-04-10 11:17:32 +0200

    fix kodi dpms service

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -31,12 +31,14 @@ in {
       };
     };
 
-    systemd.user.services.disable-dpms = {
+    systemd.services.kodi-dpms = {
       description = "Disable DPMS";
-      serviceConfig.PassEnvironment = "DISPLAY";
-      script = ''
-        sudo -u kodi bash -c 'export DISPLAY=:0; xset s off -dpms'
-      '';
+      serviceConfig = {
+        User = "kodi";
+        PassEnvironment = "DISPLAY";
+      };
+      environment = { DISPLAY = ":0"; };
+      script = "${pkgs.xorg.xset}/bin/xset s off -dpms";
       wantedBy = [ "multi-user.target" ];
     };
 
