f7fce94aadaf90743d348f05f87711c92545b248
painerp <8081128+painerp@users.noreply.github.com>
2024-05-07 04:26:51 +0200

    add immich container

---
+++ containers/immich.nix
@@ -0,0 +1,141 @@
+{ lib, config, ... }:
+
+let
+  cfg = config.server.immich;
+  config-dir = "${config.lib.server.mkConfigDir "immich"}";
+in {
+  options.server.immich = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    subdomain = lib.mkOption {
+      type = lib.types.str;
+      default = if config.server.short-subdomain then "im" else "immich";
+    };
+    auth = lib.mkOption {
+      type = lib.types.bool;
+      default = config.server.authentik.enable;
+    };
+    internal = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    env-file = lib.mkOption { type = lib.types.path; };
+    volumes = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+    version = lib.mkOption {
+      type = lib.types.str;
+      default = "release";
+    };
+    redis.image = lib.mkOption {
+      type = lib.types.str;
+      default = "redis:alpine";
+    };
+    postgres = {
+      image = lib.mkOption { type = lib.types.str; };
+      env-file = lib.mkOption { type = lib.types.path; };
+    };
+  };
+
+  config = lib.mkIf (config.modules.arion.enable && cfg.enable) {
+    systemd.services.arion-immich = {
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+    };
+
+    server.traefik.aliases =
+      config.lib.server.mkTraefikAlias { subdomain = cfg.subdomain; };
+
+    virtualisation.arion.projects.immich.settings = {
+      project.name = "immich";
+      networks.proxy.external = true;
+      networks.backend.internal = true;
+
+      services.immich-server.service = {
+        image = "ghcr.io/immich-app/immich-server:${cfg.version}";
+        container_name = "immich_server";
+        hostname = config.networking.hostName;
+        command = "start.sh immich";
+        networks = [ "proxy" "backend" ];
+        env_file = [ config.age.secrets.immich-env.path ];
+        volumes = [ "/etc/localtime:/etc/localtime:ro" ] ++ cfg.volumes;
+        depends_on = [ "database" "redis" ];
+        labels = config.lib.server.mkTraefikLabels {
+          name = "immich";
+          port = "3001";
+          subdomain = "${cfg.subdomain}";
+          forwardAuth = cfg.auth;
+        } // {
+          com.centurylinklabs.watchtower.enable = "false";
+        };
+        restart = "unless-stopped";
+      };
+
+      services.immich-microservices = {
+        out.service = {
+          deploy.resources.reservations.devices = [{
+            driver = "nvidia";
+            count = 1;
+            capabilities = [ "gpu" "compute" "video" ];
+          }];
+        };
+        service = {
+          image = "ghcr.io/immich-app/immich-server:${cfg.version}";
+          container_name = "immich_microservices";
+          hostname = config.networking.hostName;
+          command = "start.sh microservices";
+          networks = [ "backend" ];
+          env_file = [ config.age.secrets.immich-env.path ];
+          volumes = [ "/etc/localtime:/etc/localtime:ro" ] ++ cfg.volumes;
+          labels = { com.centurylinklabs.watchtower.enable = "false"; };
+          depends_on = [ "database" "redis" ];
+          restart = "unless-stopped";
+        };
+      };
+
+      services.immich-machine-learning = {
+        out.service = {
+          deploy.resources.reservations.devices = [{
+            driver = "nvidia";
+            count = 1;
+            capabilities = [ "gpu" ];
+          }];
+        };
+        service = {
+          image =
+            "ghcr.io/immich-app/immich-machine-learning:${cfg.version}-cuda";
+          container_name = "immich_machine_learning";
+          hostname = config.networking.hostName;
+          volumes = [ "${config-dir}/model-cache:/cache" ];
+          labels = { com.centurylinklabs.watchtower.enable = "false"; };
+          restart = "unless-stopped";
+        };
+      };
+
+      services.redis.service = {
+        image = "${cfg.redis.image}";
+        container_name = "immich_redis";
+        hostname = config.networking.hostName;
+        labels = { com.centurylinklabs.watchtower.enable = "false"; };
+        restart = "unless-stopped";
+      };
+
+      services.database.service = {
+        image = "${cfg.postgres.image}";
+        container_name = "immich_postgres";
+        hostname = config.networking.hostName;
+        environment = {
+          POSTGRES_USER = "postgres";
+          POSTGRES_DB = "immich";
+        };
+        env_file = [ config.age.secrets.immich-pg-env.path ];
+        volumes = [ "${config-dir}/postgres:/var/lib/postgresql/data" ];
+        labels = { com.centurylinklabs.watchtower.enable = "false"; };
+        restart = "unless-stopped";
+      };
+    };
+  };
+}
