6733a690211595324ec710a7f55aa104f65e5e08
painerp <8081128+painerp@users.noreply.github.com>
2024-06-01 04:28:00 +0200

    upgrade nixos version to 24.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "system config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     arion = {
