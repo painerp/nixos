dcaf5b12895f2c27257b7fd5a73809a5884293ed
painerp <8081128+painerp@users.noreply.github.com>
2024-06-01 04:28:00 +0200

    upgrade nixos version to 24.05

--- modules/system.nix
+++ modules/system.nix
@@ -2,5 +2,5 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
-  system.stateVersion = "23.11";
+  system.stateVersion = "24.05";
 }
