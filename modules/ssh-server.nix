76cde0f40ea739c007fbc74e69aa57b52c4b97ab
painerp <8081128+painerp@users.noreply.github.com>
2024-06-01 04:16:45 +0200

    update ssh server module

--- modules/ssh-server.nix
+++ modules/ssh-server.nix
@@ -1,10 +1,22 @@
-{
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
+{ lib, config, ... }:
+
+let cfg = config.modules.ssh;
+in {
+  options.modules.ssh = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
     };
   };
-  security.pam.enableSSHAgentAuth = true;
+
+  config = lib.mkIf (cfg.enable) {
+    services.openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
+    };
+    security.pam.sshAgentAuth.enable = true;
+  };
 }
