52de071c6d23a838d8c858837d0295e36fd790c7
painerp <8081128+painerp@users.noreply.github.com>
2024-07-11 01:46:32 +0200

    gra: update immich

--- variants/gra.nix
+++ variants/gra.nix
@@ -102,7 +102,7 @@ in {
     immich = {
       enable = true;
       auth = false;
-      version = "v1.106.4";
+      version = "v1.108.0";
       volumes = [
         "/mnt/immich:/usr/src/app/upload"
         "/mnt/nextcloud/data/painerp/files/Bilder:/library"
