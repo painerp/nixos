d0450e2467449c7483369b11d791098a2d353e6c
painerp <8081128+painerp@users.noreply.github.com>
2024-07-31 16:20:00 +0200

    add minecraft and minecraft router containers

---
+++ containers/minecraft.nix
@@ -0,0 +1,46 @@
+{ lib, config, ... }:
+
+let cfg = config.server.minecraft;
+in {
+  options.server.minecraft = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    server-type = lib.mkOption {
+      type = lib.types.str;
+      default = "PURPUR";
+    };
+  };
+
+  config = lib.mkIf (config.modules.arion.enable && cfg.enable) {
+    systemd.services.arion-minecraft = {
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+    };
+
+    virtualisation.arion.projects.minecraft.settings = {
+      project.name = "minecraft";
+      networks.proxy.external = true;
+
+      services.minecraft.service = {
+        image = "itzg/minecraft-server:latest";
+        container_name = "minecraft";
+        hostname = config.networking.hostName;
+        tty = true;
+        environment = {
+          EULA = "TRUE";
+          TYPE = cfg.server-type;
+        };
+        ports = (if (cfg.expose) then [ "25565:25565/tcp" ] else [ ])
+          ++ (if (cfg.internal) then
+            [ "${config.server.tailscale-ip}:25565:25565/tcp" ]
+          else
+            [ ]);
+        volumes = [ "${config.lib.server.mkConfigDir "minecraft"}:/data" ];
+        labels = { "com.centurylinklabs.watchtower.enable" = "true"; };
+        restart = "unless-stopped";
+      };
+    };
+  };
+}
