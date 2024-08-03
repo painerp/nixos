f55c928de6996dc63f74b37a0a93501906af6957
painerp <8081128+painerp@users.noreply.github.com>
2024-08-03 03:06:29 +0200

    add minecraft env file and rcon support

--- containers/minecraft.nix
+++ containers/minecraft.nix
@@ -15,13 +15,30 @@ in {
       type = lib.types.bool;
       default = !cfg.expose;
     };
+    env-file = lib.mkOption { type = lib.types.path; };
     server-type = lib.mkOption {
       type = lib.types.str;
       default = "PURPUR";
     };
+    rcon = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+      expose = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+      internal = lib.mkOption {
+        type = lib.types.bool;
+        default = !cfg.rcon.expose;
+      };
+    };
   };
 
   config = lib.mkIf (config.modules.arion.enable && cfg.enable) {
+    age.secrets.minecraft-env.file = cfg.env-file;
+
     systemd.services.arion-minecraft = {
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
@@ -29,7 +46,6 @@ in {
 
     virtualisation.arion.projects.minecraft.settings = {
       project.name = "minecraft";
-      networks.proxy.external = true;
 
       services.minecraft.service = {
         image = "itzg/minecraft-server:latest";
@@ -45,7 +61,13 @@ in {
           ++ (if (cfg.internal) then
             [ "${config.server.tailscale-ip}:25565:25565/tcp" ]
           else
-            [ ]);
+            [ ]) ++ (if (cfg.rcon.enable && cfg.rcon.expose) then
+              [ "25575:25575/tcp" ]
+            else
+              [ ]) ++ (if (cfg.rcon.enable && cfg.rcon.internal) then
+                [ "${config.server.tailscale-ip}:25575:25575/tcp" ]
+              else
+                [ ]);
         volumes = [ "${config.lib.server.mkConfigDir "minecraft"}:/data" ];
         labels = { "com.centurylinklabs.watchtower.enable" = "false"; };
         restart = "unless-stopped";
