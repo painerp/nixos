7485222cecb8cfb71475e7baefdd65a1a928df82
painerp <8081128+painerp@users.noreply.github.com>
2024-08-10 04:00:22 +0200

    add more package options for desktop support

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,13 +1,80 @@
-{ pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
-{
-  environment.systemPackages = with pkgs; [
-    vim
-    nano
-    dig
-    htop
-    git
-    home-manager
-    ncdu
-  ];
+let
+  cfg = config.modules.packages;
+  makeOption = defaultVal:
+    lib.mkOption {
+      type = lib.types.bool;
+      default = if cfg.full then true else defaultVal;
+    };
+in {
+  options.modules.packages = {
+    full = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    video = makeOption false;
+    office = makeOption false;
+    desktop = makeOption false;
+    gaming = makeOption false;
+    communication = makeOption false;
+    dev = makeOption false;
+    crypto = makeOption false;
+  };
+
+  config = {
+    environment.systemPackages = with pkgs;
+      [ vim nano eza dig htop git home-manager ncdu jq ffmpeg zoxide ]
+      ++ (if cfg.desktop then [
+        btop
+        brave
+        firefox
+        librewolf
+        nextcloud-client
+        nomacs
+        syncthing
+        vlc
+        vorta
+        keepassxc
+        xfce.thunar
+        xfce.thunar-archive-plugin
+        xfce.thunar-volman
+      ] else
+        [ ]) ++ (if cfg.video then [
+          freetube
+          handbrake
+          kdenlive
+          yt-dlp
+          obs-studio
+          obs-studio-plugins.advanced-scene-switcher
+        ] else
+          [ ]) ++ (if cfg.office then [ libreoffice okular ] else [ ])
+      ++ (if cfg.gaming then [
+        lutris
+        proton-ge-bin
+        steamguard-cli
+        prismlauncher
+      ] else
+        [ ]) ++ (if cfg.tor then [ tor nyx protonvpn-gui ] else [ ])
+      ++ (if cfg.communication then [
+        teamspeak-client
+        webcord
+        signal-desktop
+      ] else
+        [ ]) ++ (if cfg.dev then [
+          gimp
+          lazygit
+          vscodium
+          jetbrains.webstorm
+          jetbrains.rust-rover
+          jetbrains.pycharm-professional
+          jetbrains.idea-ultimate
+        ] else
+          [ ]) ++ (if cfg.crypto then [ electrum monero-gui ] else [ ]);
+  };
+
+  programs = {
+    steam.enable = cfg.gaming;
+    kdeconnect.enable = cfg.desktop;
+  };
 }
