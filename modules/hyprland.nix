18151418d164eee707986c35740f360ce1649220
painerp <8081128+painerp@users.noreply.github.com>
2024-09-06 00:56:08 +0200

    hyprland: add kde polkit

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -78,6 +78,21 @@ in {
       #      };
       gnome.gnome-keyring.enable = true;
     };
-    services.hypridle.enable = true;
+
+    security.polkit.enable = true;
+
+    systemd.user.services.polkit-kde-authentication-agent-1 = {
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
   };
 }
