c2ca5dd883b9eb4f62ff52c1661fd1265b1bf961
painerp <8081128+painerp@users.noreply.github.com>
2024-09-06 01:00:29 +0200

    lenovo-15arh05h: add bootloader, add tlp, add sound & bluetooth support

--- hardware/lenovo-15arh05h.nix
+++ hardware/lenovo-15arh05h.nix
@@ -1,22 +1,70 @@
-{ inputs, ... }:
+{ config, inputs, pkgs, lib, modulesPath, ... }:
 
 {
-  imports = [ inputs.nixos-hardware.nixosModules.lenovo-legion-15arh05h ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    inputs.nixos-hardware.nixosModules.lenovo-legion-15arh05h
+  ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # nvidia
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+  boot = {
+    initrd.availableKernelModules =
+      [ "xhci_pci" "nvme" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    kernelPackages = pkgs.linuxPackages_6_9;
+    kernelModules = [ "kvm-amd" ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
   };
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    open = false;
-    nvidiaSettings = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  security.rtkit.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      #jack.enable = true;
+    };
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        PLATFORM_PROFILE_ON_AC = "balanced";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 0;
+        USB_EXCLUDE_BTUSB = 1;
+        USB_EXCLUDE_PHONE = 1;
+        WOL_DISABLE = "Y";
+      };
+    };
+  };
+
+  hardware = {
+    cpu.amd.updateMicrocode =
+      lib.mkDefault config.hardware.enableRedistributableFirmware;
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    nvidia = {
+      modesetting.enable = true;
+      #    powerManagement.enable = false;
+      open = false;
+      #    nvidiaSettings = false;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings.General.Experimental = true; # for gnome-bluetooth percentage
+    };
   };
 }
