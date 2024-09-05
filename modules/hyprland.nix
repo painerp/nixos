18f6a3722e762b14dc0d06c1e76655b9bfe09976
painerp <8081128+painerp@users.noreply.github.com>
2024-09-06 00:54:11 +0200

    hyprland: add greetd (for now)

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -10,6 +10,23 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    #    services = {
+    #      displayManager.sddm = {
+    #        enable = true;
+    #        wayland.enable = true;
+    #      };
+    #      xserver.enable = true;
+    #    };
+
+    services.greetd = {
+      enable = true;
+      settings.default_session = {
+        command =
+          "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+
     environment = with pkgs; {
       systemPackages = [
         udiskie
