cf5160d8f8875450dd74aa1a8c0a3de4563d9d94
painerp <8081128+painerp@users.noreply.github.com>
2024-09-06 01:11:15 +0200

    kronos: update filesystems

--- variants/kronos.nix
+++ variants/kronos.nix
@@ -6,13 +6,26 @@ in {
 
   networking = { hostName = "${flake}"; };
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/C34B-E9C0";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
+
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/ae496939-fea4-4d31-a15e-08ca064478aa";
-    fsType = "btrfs";
+    device = "/dev/disk/by-uuid/ce9ee924-0948-4ac1-83d4-8b10731b21ba";
+    fsType = "ext4";
   };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/4a6b0055-10ec-45b7-b07e-3cfcb2a57915"; }];
+    [{ device = "/dev/disk/by-uuid/040cc9dd-71cf-4c88-8cfa-cf52e21bc9e5"; }];
+
+  boot.initrd.luks.devices = {
+    "luks-1fea4deb-b91d-46cb-9fc2-abb4e15cb2c6".device =
+      "/dev/disk/by-uuid/1fea4deb-b91d-46cb-9fc2-abb4e15cb2c6";
+    "luks-97fed62b-bdc0-43b3-b670-ed83775644aa".device =
+      "/dev/disk/by-uuid/97fed62b-bdc0-43b3-b670-ed83775644aa";
+  };
 
   fileSystems."/mnt/backup" = {
     device = "100.115.213.122:/mnt/hdd/backup/kronos";
