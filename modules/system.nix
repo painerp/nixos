5b22b382c50da6912dc688ad38cff6c59e991905
painerp <8081128+painerp@users.noreply.github.com>
2024-09-06 00:50:01 +0200

    disable warn dirty in nix settings

--- modules/system.nix
+++ modules/system.nix
@@ -1,5 +1,8 @@
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    warn-dirty = false;
+  };
   console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
   system.stateVersion = "24.05";
