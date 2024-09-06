0ac437ce2ed9d78d896fff29bb95277ed7229a11
painerp <8081128+painerp@users.noreply.github.com>
2024-09-06 05:32:20 +0200

    remove configuration.nix

--- configuration.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  hardware.enableRedistributableFirmware = true;
-  system.stateVersion = "23.11";
-}
