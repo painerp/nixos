a560b0cb508b36e10cc5807d2398170de2d93b5f
painerp <8081128+painerp@users.noreply.github.com>
2024-10-13 00:48:56 +0200

    nvidia: fix docker enabling with deprecated docker option for now

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -41,6 +41,7 @@ in
           cfg.package;
       };
 
+      virtualisation.docker.enableNvidia = config.modules.arion.enable;
       nvidia-container-toolkit.enable = config.modules.arion.enable;
     };
   };
