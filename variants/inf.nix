377b152e64986a52a5b3f61db613a75cb67981c5
painerp <8081128+painerp@users.noreply.github.com>
2024-10-18 12:09:25 +0200

    inf: add home-assistant

--- variants/inf.nix
+++ variants/inf.nix
@@ -48,6 +48,7 @@ in {
       env-file = secrets.inf-jellystat-env;
       postgres.env-file = secrets.inf-jellystat-pg-env;
     };
+    home-assistant.enable = true;
     linkwarden = {
       enable = true;
       env-file = secrets.inf-linkwarden-env;
