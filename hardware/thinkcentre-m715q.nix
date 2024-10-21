dcbfd3348d139c17a36d4426c4986a2d1c5df4c8
painerp <8081128+painerp@users.noreply.github.com>
2024-10-21 16:33:13 +0200

    hardware: add thinkcentre m715q gen 2 support

---
+++ hardware/thinkcentre-m715q.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot = {
+    initrd.availableKernelModules =
+      [ "ehci_pci" "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    kernelModules = [ "kvm-amd" ];
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    #jack.enable = true;
+  };
+
+  hardware.cpu.amd.updateMicrocode = true;
+}
