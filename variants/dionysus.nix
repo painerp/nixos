4fe993b7cfb3591994b9d88952d4a78a9ba4890d
painerp <8081128+painerp@users.noreply.github.com>
2024-10-31 15:26:23 +0100

    variants: add dionysus

---
+++ variants/dionysus.nix
@@ -0,0 +1,118 @@
+{
+  inputs,
+  pkgs,
+  config,
+  modulesPath,
+  secrets,
+  lib,
+  ...
+}:
+
+let
+  flake = "dionysus";
+  truenas-ip = "100.111.75.128";
+in
+{
+  imports = [ ./secrets ];
+
+  networking = {
+    hostName = "${flake}";
+    useDHCP = lib.mkDefault true;
+    networkmanager.enable = true;
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/C34B-E9C0";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/ce9ee924-0948-4ac1-83d4-8b10731b21ba";
+    fsType = "ext4";
+  };
+
+  swapDevices = [ { device = "/dev/disk/by-uuid/040cc9dd-71cf-4c88-8cfa-cf52e21bc9e5"; } ];
+
+  boot.initrd.luks.devices = {
+    "luks-1fea4deb-b91d-46cb-9fc2-abb4e15cb2c6".device = "/dev/disk/by-uuid/1fea4deb-b91d-46cb-9fc2-abb4e15cb2c6";
+    "luks-97fed62b-bdc0-43b3-b670-ed83775644aa".device = "/dev/disk/by-uuid/97fed62b-bdc0-43b3-b670-ed83775644aa";
+  };
+
+  fileSystems."/mnt/backup-arch" = {
+    device = "${truenas-ip}:/mnt/hdd/backup/archlinux";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
+
+  fileSystems."/mnt/backup" = {
+    device = "${truenas-ip}:/mnt/hdd/backup/dionysus";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
+
+  fileSystems."/mnt/media" = {
+    device = "${truenas-ip}:/mnt/hdd/media";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
+
+  fileSystems."/mnt/unknown" = {
+    device = "${truenas-ip}:/mnt/hdd/unknown";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
+
+  fileSystems."/mnt/rebuild" = {
+    device = "${truenas-ip}:/mnt/hdd/rebuild";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
+
+  # system
+  system = {
+    inherit flake;
+    username = flake;
+  };
+  modules = {
+    ssh.enable = false;
+    arion.enable = true;
+    hyprland.enable = true;
+    packages.full = true;
+    amd.enable = true;
+  };
+
+  # users
+  users.mutableUsers = false;
+  users.users."${flake}" = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "audio"
+      "video"
+      "input"
+      "docker"
+    ];
+  };
+
+  nix.settings.trusted-users = [ "@wheel" ];
+}
