71d6853dadf0a17675f9d3657dab74dfd5773ccc
painerp <8081128+painerp@users.noreply.github.com>
2024-10-31 19:59:03 +0100

    packages: reformat & add java and vm support

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,13 +1,21 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 
 let
   cfg = config.modules.packages;
-  makeOption = defaultVal:
+  makeOption =
+    defaultVal:
     lib.mkOption {
       type = lib.types.bool;
       default = if cfg.full then true else defaultVal;
     };
-in {
+in
+{
   options.modules.packages = {
     full = lib.mkOption {
       type = lib.types.bool;
@@ -22,60 +30,135 @@ in {
     communication = makeOption false;
     dev = makeOption false;
     crypto = makeOption false;
+    vm = makeOption false;
   };
 
   config = {
-    environment.systemPackages = with pkgs;
-      [ vim nano eza dig htop git home-manager ncdu jq ffmpeg zoxide tldr ]
-      ++ (if cfg.desktop then [
-        btop
-        brave
-        firefox
-        librewolf
-        nextcloud-client
-        nomacs
-        kdePackages.kate
-        kdePackages.ark
-        vlc
-        vorta
-        keepassxc
-      ] else
-        [ ]) ++ (if cfg.video then [
-          freetube
-          handbrake
-          kdenlive
-          yt-dlp
-          (pkgs.wrapOBS {
-            plugins = with pkgs.obs-studio-plugins; [ advanced-scene-switcher ];
-          })
-        ] else
-          [ ]) ++ (if cfg.office then [ libreoffice okular obsidian ] else [ ])
-      ++ (if cfg.image then [ krita upscayl ] else [ ]) ++ (if cfg.gaming then [
-        mangohud
-        heroic
-        lutris
-        protonup
-        steamguard-cli
-        prismlauncher
-      ] else
-        [ ]) ++ (if cfg.tor then [ tor nyx protonvpn-gui ] else [ ])
-      ++ (if cfg.communication then [
-        teamspeak_client
-        webcord
-        signal-desktop
-      ] else
-        [ ]) ++ (if cfg.dev then [
-          nixfmt-rfc-style
-          lazygit
-          nodePackages_latest.nodejs
-          nodePackages_latest.pnpm
-          python3
-          jetbrains.webstorm
-          jetbrains.rust-rover
-          jetbrains.pycharm-professional
-          jetbrains.idea-ultimate
-        ] else
-          [ ]) ++ (if cfg.crypto then [ electrum monero-gui ] else [ ]);
+    environment.systemPackages =
+      with pkgs;
+      [
+        vim
+        nano
+        eza
+        dig
+        htop
+        git
+        home-manager
+        ncdu
+        jq
+        ffmpeg
+        zoxide
+        tldr
+      ]
+      ++ (
+        if cfg.desktop then
+          [
+            btop
+            brave
+            firefox
+            librewolf
+            nextcloud-client
+            nomacs
+            kdePackages.kate
+            kdePackages.ark
+            vlc
+            vorta
+            keepassxc
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.video then
+          [
+            pkgs-unstable.freetube
+            handbrake
+            kdenlive
+            yt-dlp
+            (pkgs.wrapOBS { plugins = with pkgs.obs-studio-plugins; [ advanced-scene-switcher ]; })
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.office then
+          [
+            libreoffice
+            okular
+            obsidian
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.image then
+          [
+            krita
+            upscayl
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.gaming then
+          [
+            mangohud
+            heroic
+            lutris
+            protonup
+            steamguard-cli
+            prismlauncher
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.tor then
+          [
+            tor
+            nyx
+            protonvpn-gui
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.communication then
+          [
+            teamspeak_client
+            webcord
+            signal-desktop
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.dev then
+          [
+            ungoogled-chromium
+            nixfmt-rfc-style
+            lazygit
+            nodePackages_latest.nodejs
+            nodePackages_latest.pnpm
+            python3
+            jdk
+            jetbrains.webstorm
+            jetbrains.rust-rover
+            jetbrains.pycharm-professional
+            jetbrains.idea-ultimate
+          ]
+        else
+          [ ]
+      )
+      ++ (
+        if cfg.crypto then
+          [
+            electrum
+            monero-gui
+          ]
+        else
+          [ ]
+      );
 
     programs = {
       steam = {
@@ -87,7 +170,10 @@ in {
       xfconf.enable = cfg.desktop;
       thunar = {
         enable = cfg.desktop;
-        plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+        plugins = with pkgs.xfce; [
+          thunar-archive-plugin
+          thunar-volman
+        ];
       };
       nh = {
         enable = true;
@@ -95,7 +181,14 @@ in {
         #        clean.extraArgs = "--keep-since 4d --keep 3";
         flake = "/etc/nixos";
       };
+      java = {
+        enable = cfg.dev;
+        package = pkgs.jdk;
+      };
     };
+
+    virtualisation.virtualbox.host.enable = cfg.vm;
+    users.extraGroups.vboxusers.members = [ "${config.system.username}" ];
 
     services = {
       tumbler.enable = cfg.desktop;
