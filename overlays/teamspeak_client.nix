7d3b5039024d1d7e4327876087d7f2de5e5a4fa3
painerp <8081128+painerp@users.noreply.github.com>
2024-11-01 23:51:16 +0100

    teamspeak: fix fetch zips hash and extraction

--- overlays/teamspeak_client.nix
+++ overlays/teamspeak_client.nix
@@ -1,18 +1,27 @@
-{ pkgs, lib, ... }:
+{ }:
 
 final: prev: rec {
   teamspeak_client = prev.teamspeak_client.overrideAttrs (oldAttrs: rec {
-    soundpack = lib.fetchzip {
+    soundpack = final.fetchzip {
+      name = "Teamspeak_DukeNukem_Soundpack";
       url = "https://addons-content.teamspeak.com/8a9a93a7-5dd6-403e-8d15-435e5bfbd970/files/3/dukenukem-2.0.0.ts3_soundpack";
-      hash = "341199c11963c0773f20bbf81824d8c3e9296bf989b6657ad489f76689449941";
+      hash = "sha256-tArylFzkMgb7u2oSmEzxsongWyxBfIjkzqNqiYFd+LQ=";
+      extension = "zip";
+      stripRoot = false;
     };
-    iconpack = lib.fetchzip {
+    iconpack = final.fetchzip {
+      name = "Teamspeak_DarkenTS_Icons";
       url = "https://addons-content.teamspeak.com/0b57d54d-b46c-433d-8f7e-2eea28470007/files/8/DarkenTS_v1.7.1.ts3_iconpack";
-      hash = "9b26447f3d5bb4a54422a8c6903628cb167a02d35b2ad8a75f4090964e68ee50";
+      hash = "sha256-nWdULV+oY7/XbA3lArPPKu0woe18aE/McmcI5DPfTL8=";
+      extension = "zip";
+      stripRoot = false;
     };
-    style = lib.fetchzip {
+    style = final.fetchzip {
+      name = "Teamspeak_DarkenTS_Style";
       url = "https://addons-content.teamspeak.com/686209af-0b66-4805-b2d7-0e990f7cb9e0/files/15/DarkenTSv172_5ec97131ef925.ts3_style";
-      hash = "77bcaa82e01646383871c0647f4f86195b3ca0ac1d2cddae1b7f18a331601f2f";
+      hash = "sha256-CztGAxuE1x00Qhw38Lzasg3qyLrgpxNNKGsmWZo5U80=";
+      extension = "zip";
+      stripRoot = false;
     };
     installPhase =
       oldAttrs.installPhase
