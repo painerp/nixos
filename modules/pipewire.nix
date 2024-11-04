fc9ae7fade3dc2ea960633291bac906874de7240
painerp <8081128+painerp@users.noreply.github.com>
2024-11-04 05:33:24 +0100

    pipewire: fix combine sink

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -9,9 +9,15 @@ in
       type = lib.types.bool;
       default = false;
     };
-    audiosink = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
+    audiosink = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+      output = lib.mkOption {
+        type = lib.types.str;
+        required = cfg.audiosink.enable;
+      };
     };
   };
 
@@ -24,38 +30,57 @@ in
         pulse.enable = true;
         #jack.enable = true;
       }
-      // lib.attrsets.optionalAttrs (cfg.audiosink) {
+      // lib.attrsets.optionalAttrs (cfg.audiosink.enable) {
         extraConfig.pipewire = {
-          "10-null-sink".context.objects = {
-            factory = "adapter";
-            args = {
-              node.name = "null_sink";
-              media.class = "Audio/Sink";
-              audio.position = [
-                FL
-                FR
-              ];
-              factory.name = "support.null-audio-sink";
-              node.description = "Virtual Null Sink";
-            };
-          };
-        };
-        "20-combine-sink".context.objects = {
-          factory = "adapter";
-          args = {
-            node.name = "combined_sink";
-            media.class = "Audio/Sink";
-            audio.position = [
-              FL
-              FR
-            ];
-            factory.name = "combine-sink";
-            node.description = "Combined Output";
-            combine.sinks = [
-              { target.object = "@DEFAULT_AUDIO_SINK@"; }
-              { target.object = "null_sink"; }
-            ];
-          };
+          "10-null-sink"."context.objects" = [
+            {
+              factory = "adapter";
+              args = {
+                "node.name" = "null_sink";
+                "media.class" = "Audio/Sink";
+                "audio.position" = "FL,FR";
+                "factory.name" = "support.null-audio-sink";
+                "node.description" = "Null Sink";
+              };
+            }
+          ];
+          "20-combine-sink"."context.modules" = [
+            {
+              name = "libpipewire-module-combine-stream";
+              args = {
+                "node.name" = "combined_sink";
+                "factory.name" = "combine-sink";
+                "node.description" = "Combined Sink";
+                "combine.latency-compensate" = false;
+                "combine.probs" = {
+                  "audio.position" = "FL,FR";
+                };
+                "stream.props" = {
+                  stream.dont-remix = true;
+                };
+                "stream.rules" = [
+                  {
+                    matches = [ { "node.name" = "null_sink"; } ];
+                    actions = {
+                      create-stream = {
+                        "combine.audio.position" = "FL,FR";
+                        "audio.position" = "FL,FR";
+                      };
+                    };
+                  }
+                  {
+                    matches = [ { "node.name" = "${cfg.audiosink.output}"; } ];
+                    actions = {
+                      create-stream = {
+                        "combine.audio.position" = "FL,FR";
+                        "audio.position" = "FL,FR";
+                      };
+                    };
+                  }
+                ];
+              };
+            }
+          ];
         };
       };
   };
