4d3b9808d2fe3a09a3b4686f512b5b8df952ab06
painerp <8081128+painerp@users.noreply.github.com>
2024-11-20 14:03:54 +0100

    pkgs: fix bin paths

--- pkgs/toggle-refresh.nix
+++ pkgs/toggle-refresh.nix
@@ -21,16 +21,16 @@ in
       (pkgs.writeShellScriptBin "toggle-refresh" ''
         MONITORS=$(hyprctl monitors -j)
 
-        if [ "$(echo "$MONITORS" | ${pkgs.jq} length)" -gt 1 ]; then
+        if [ "$(echo "$MONITORS" | ${pkgs.jq}/bin/jq length)" -gt 1 ]; then
           echo "Only one monitor is supported"
           exit 1
         fi
 
-        MONITOR=$(echo "$MONITORS" | ${pkgs.jq} -r .[0].name)
-        CURRENT_RATE=$(echo "$MONITORS" | ${pkgs.jq} -r .[0].refreshRate)
+        MONITOR=$(echo "$MONITORS" | ${pkgs.jq}/bin/jq -r .[0].name)
+        CURRENT_RATE=$(echo "$MONITORS" | ${pkgs.jq}/bin/jq -r .[0].refreshRate)
 
         LOW_RATE="60"
-        HIGH_RATE="$(echo "$MONITORS" | ${pkgs.jq} -r .[0].availableModes.[0] | sed 's/.*@\(.*\)Hz/\1/')"
+        HIGH_RATE="$(echo "$MONITORS" | ${pkgs.jq}/bin/jq -r .[0].availableModes.[0] | sed 's/.*@\(.*\)Hz/\1/')"
 
         if [[ $(printf "%.0f" "$CURRENT_RATE") -eq "$LOW_RATE" ]]; then
           hyprctl keyword monitor "$MONITOR",1920x1080@"$HIGH_RATE",auto,1
