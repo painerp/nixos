443c9a0d7c4e6d6af0fcf5307f634d67ed6f3460
painerp <8081128+painerp@users.noreply.github.com>
2024-11-22 04:11:43 +0100

    hyprland: enable greetd gnome keyring and remove kde polkit

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -96,19 +96,9 @@ in
       gnome.gnome-keyring.enable = true;
     };
 
-    security.polkit.enable = true;
-
-    systemd.user.services.polkit-kde-authentication-agent-1 = {
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
+    security = {
+      polkit.enable = true;
+      pam.services.greetd.enableGnomeKeyring = true;
     };
   };
 }
