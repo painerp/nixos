94bfee61877121b3888bd8071fcded0762713486
painerp <8081128+painerp@users.noreply.github.com>
2024-11-23 12:09:32 +0100

    system: enable new scx service

--- modules/system.nix
+++ modules/system.nix
@@ -7,6 +7,7 @@
     warn-dirty = false;
     auto-optimise-store = true;
   };
+  services.scx.enable = true;
   console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
   system.stateVersion = "24.11";
