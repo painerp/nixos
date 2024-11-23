cd5493ed90e88917077914f6d1ffcaaa68b1579d
painerp <8081128+painerp@users.noreply.github.com>
2024-11-23 11:36:14 +0100

    system: add auto optimise store and remove the optimise module

--- modules/optimize.nix
+++
@@ -1,9 +0,0 @@
-{
-  nix.settings.auto-optimise-store = true;
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    randomizedDelaySec = "60min";
-    options = "--delete-older-than 30d";
-  };
-}
