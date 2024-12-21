5bfd1a40ccd4257af41f11c9dafd2c37799da7bc
painerp <8081128+painerp@users.noreply.github.com>
2024-12-21 19:45:02 +0100

    jellyfin: update gpu config

--- containers/jellyfin.nix
+++ containers/jellyfin.nix
@@ -43,8 +43,8 @@ in
         out.service = {
           deploy.resources.reservations.devices = [
             {
-              driver = "nvidia";
-              count = 1;
+              driver = "cdi";
+              device_ids = [ "nvidia.com/gpu=all" ];
               capabilities = [ "gpu" ];
             }
           ];
