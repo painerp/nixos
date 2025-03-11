cd18a73b0d7e597c5771cf9c089f0f88e564d1fc
painerp <8081128+painerp@users.noreply.github.com>
2025-03-11 12:58:47 +0100

    variants: add sex

---
+++ variants/sex.nix
@@ -0,0 +1,135 @@
+{
+  pkgs,
+  config,
+  modulesPath,
+  secrets,
+  lib,
+  ...
+}:
+
+let
+  flake = "sex";
+  tailscale-ip = "100.114.178.125";
+in
+{
+  imports = [
+    ./secrets
+    ./secrets/sex.nix
+  ];
+
+  networking.hostName = "nix${flake}";
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/ea881f84-b512-47e8-af97-4260a2fb4e51";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/E900-FD3F";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ { device = "/dev/disk/by-uuid/55f7d97a-a438-4ac9-8909-2e08a78e3ef5"; } ];
+
+  # system
+  system = {
+    inherit flake;
+  };
+  modules = {
+    arion.enable = true;
+  };
+
+  # services
+  server = {
+    base-domain = "redacted";
+    inherit tailscale-ip;
+    short-subdomain = true;
+    adguardhome.enable = true;
+    authentik = {
+      enable = true;
+      subdomain = "auth";
+      env-file = secrets.ext-authentik-env;
+      postgres.env-file = secrets.ext-authentik-pg-env;
+    };
+    gotify = {
+      enable = true;
+      subdomain = "got";
+      auth = false;
+    };
+    monitoring = {
+      node-exporter.enable = true;
+      cadvisor.enable = true;
+      promtail = {
+        enable = true;
+        loki.address = "100.73.203.96";
+      };
+    };
+    nuxt-pages = {
+      mysql.env-file = secrets.ext-nuxt-pages-mysql-env;
+      pma = {
+        enable = true;
+        env-file = secrets.ext-nuxt-pages-pma-env;
+      };
+      app = {
+        enable = true;
+        root = true;
+        auth = false;
+        image = "redacted";
+        env-file = secrets.ext-nuxt-pages-env;
+      };
+    };
+    open-webui = {
+      enable = true;
+      subdomain = "chat";
+      auth = false;
+      env-file = secrets.ext-open-webui-env;
+    };
+    minecraft-router = {
+      enable = true;
+      expose = true;
+    };
+    teamspeak = {
+      enable = true;
+      expose = true;
+      env-file = secrets.ext-teamspeak-env;
+    };
+    tsmusicbot.enable = true;
+    traefik = {
+      enable = true;
+      subdomain = "t";
+      expose = true;
+      extra-entrypoints = {
+        talk-tcp.address = ":3478";
+        talk-udp.address = ":3478/udp";
+        # palworld-udp.address = ":8211/udp";
+      };
+      extra-ports = [
+        "3478:3478/tcp"
+        "3478:3478/udp"
+        # "8211:8211/udp"
+      ];
+    };
+    goaccess.enable = true;
+    nginx = {
+      enable = true;
+      rule = "Host(`jf.${config.server.domain}`) && PathPrefix(`/extras`)";
+      middleware = "strip-extras-prefix";
+      labels = {
+        "traefik.http.middlewares.strip-extras-prefix.stripPrefix.prefixes" = "/extras";
+      };
+      auth = false;
+    };
+    uptime-kuma = {
+      enable = true;
+      subdomain = "st";
+    };
+    watchtower = {
+      enable = true;
+      internal-services = true;
+    };
+  };
+
+  # users
+  users.mutableUsers = false;
+  nix.settings.trusted-users = [ "@wheel" ];
+}
