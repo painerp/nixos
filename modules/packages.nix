328502582a710303cbd7b37d004a67138c2c2173
painerp <8081128+painerp@users.noreply.github.com>
2025-04-01 12:20:46 +0200

    packages: fix apod-wallpapers systemd file to wait for the network to be running before starting

--- modules/packages.nix
+++ modules/packages.nix
@@ -264,6 +264,8 @@ in
 
     systemd.user = lib.mkIf (cfg.desktop) {
       services.apod-wallpaper = {
+        Wants = [ "network-online.target" ];
+        After = [ "network-online.target" ];
         serviceConfig.Type = "oneshot";
         script = ''
           export PATH=${
@@ -279,6 +281,8 @@ in
       };
       timers.apod-wallpaper = {
         wantedBy = [ "default.target" ];
+        Wants = [ "network-online.target" ];
+        After = [ "network-online.target" ];
         timerConfig = {
           OnCalendar = "*-*-* 07:15:00 ${config.time.timeZone}";
           Persistent = true;
