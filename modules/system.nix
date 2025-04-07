8bc22ed6ab6831b01aa268dd334c3684a312f9e4
painerp <8081128+painerp@users.noreply.github.com>
2025-04-08 01:29:25 +0200

    system: add language options

--- modules/system.nix
+++ modules/system.nix
@@ -7,7 +7,27 @@
     warn-dirty = false;
     auto-optimise-store = true;
   };
+
   console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+    ];
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
+
   system.stateVersion = "24.11";
 }
