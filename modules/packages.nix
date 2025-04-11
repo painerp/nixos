03648fe6d3b00eeaa44bc546778bb35d5d1a7fd3
painerp <8081128+painerp@users.noreply.github.com>
2025-04-11 18:11:38 +0200

    packages: add flake to nh

--- modules/packages.nix
+++ modules/packages.nix
@@ -199,7 +199,7 @@ in
         enable = true;
         clean.enable = true;
         clean.extraArgs = "--keep-since 30d --keep 30";
-        flake = "/etc/nixos";
+        flake = "/etc/nixos#${config.system.flake}";
       };
       java = {
         enable = cfg.dev;
