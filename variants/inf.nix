a9198e0990b629069002a696365d8be3dbe82e11
painerp <8081128+painerp@users.noreply.github.com>
2025-05-03 17:39:48 +0200

    inf: update dawarich to v0.24.0

--- variants/inf.nix
+++ variants/inf.nix
@@ -76,7 +76,7 @@ in
     };
     dawarich = {
       enable = true;
-      version = "0.23.5";
+      version = "0.24.0";
       env-file = secrets.inf-dawarich-env;
       postgres.env-file = secrets.inf-dawarich-pg-env;
     };
