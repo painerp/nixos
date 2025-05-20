962d13c721f5dcc599c8488578737fd90d59c4e9
painerp <8081128+painerp@users.noreply.github.com>
2025-05-20 20:51:52 +0200

    nix: update to 25.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "system config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     arion = {
@@ -20,7 +20,7 @@
     apod-wallpaper.url = "github:painerp/apod-wallpaper";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
