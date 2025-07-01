f7bb5da4ec08c0451214e8a6c86ed71b47a29f5d
painerp <8081128+painerp@users.noreply.github.com>
2025-07-01 19:25:23 +0200

    pkgs: improve ice-connect error handling

--- pkgs/ice-connect.nix
+++ pkgs/ice-connect.nix
@@ -24,14 +24,28 @@ in
             nmcli dev wifi connect WIFIonICE
         fi
 
-        # get ip using dig for "iceportal.de" from the dns provider given by the network interface
-        DNS_IP=$(ip route | grep default | awk '{print $3}')
-        ICEPORTALIP=$(dig +short iceportal.de "@$DNS_IP" | head -n 1)
-        WIFIONICEIP=$(dig +short login.wifionice.de "@$DNS_IP" | head -n 1)
+        # check if db network is already used 172.18.0.1 for example by docker
+        ASSIGNED_IPS=$(ip -j a | jq -r '.[].addr_info[].local')
+        if echo "$ASSIGNED_IPS" | grep -q "^172.18.0.1$"; then
+            echo "Error: IP 172.18.0.1 is already assigned to an interface"
+            exit 1
+        fi
 
-        # check if ips are not empty else quit
-        if [ -z "$ICEPORTALIP" ] || [ -z "$WIFIONICEIP" ]; then
-            echo "Could not find IP address of iceportal.de or login.wifionice.de"
+        validate_ip() {
+            local ip=$1
+            local regex='^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+
+            [[ $ip =~ $regex ]]
+        }
+
+        # get ip using dig for "iceportal.de" and "login.wifionice.de" from the dns provider given by the network interface
+        DNS_IP=$(ip route | grep default | awk '{print $3}')
+        ICEPORTALIP=$(dig +short +tries=10 iceportal.de "@$DNS_IP" | grep -v "^;;" | head -n 1)
+        WIFIONICEIP=$(dig +short +tries=10 login.wifionice.de "@$DNS_IP" | grep -v "^;;" | head -n 1)
+
+        # check if ips are not empty or not valid ips else quit
+        if [ -z "$ICEPORTALIP" ] || ! validate_ip "$ICEPORTALIP" || [ -z "$WIFIONICEIP" ] || ! validate_ip "$WIFIONICEIP" ; then
+            echo "Could not find valid IP address of iceportal.de or login.wifionice.de"
             exit 1
         fi
 
