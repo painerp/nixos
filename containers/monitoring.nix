3162d90000f6f99744c2319cd321724850936586
painerp <8081128+painerp@users.noreply.github.com>
2025-08-28 16:51:47 +0200

    monitoring: update prometheus retention to 90d

--- containers/monitoring.nix
+++ containers/monitoring.nix
@@ -225,13 +225,14 @@ in
                 container_name = "prometheus";
                 networks = [
                   "proxy"
-                ] ++ (if (cfg.cadvisor.enable || cfg.node-exporter.enable) then [ "exporter" ] else [ ]);
+                ]
+                ++ (if (cfg.cadvisor.enable || cfg.node-exporter.enable) then [ "exporter" ] else [ ]);
                 user = "0:0";
                 command = [
                   "--web.enable-admin-api"
                   "--config.file=/etc/prometheus/prometheus.yml"
                   "--storage.tsdb.path=/prometheus"
-                  "--storage.tsdb.retention.time=1y"
+                  "--storage.tsdb.retention.time=90d"
                   "--web.console.libraries=/usr/share/prometheus/console_libraries"
                   "--web.console.templates=/usr/share/prometheus/consoles"
                 ];
