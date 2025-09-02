f0b92777656c5b242414662cd479de2a17fe1c65
painerp <8081128+painerp@users.noreply.github.com>
2025-09-02 18:24:06 +0200

    inf: add authentik version

--- variants/inf.nix
+++ variants/inf.nix
@@ -55,6 +55,7 @@ in
     authentik = {
       enable = true;
       proxy = true;
+      version = "2025.8.1";
       env-file = secrets.inf-authentik-proxy-env;
     };
     dashboard.enable = true;
