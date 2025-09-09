71a18ca5b3ef66f40c0c72ded3452d99512cbd79
painerp <8081128+painerp@users.noreply.github.com>
2025-09-10 00:53:04 +0200

    tux-ibp-14-gen10: enable tuxedo-rs

--- hardware/tuxedo-infinitybook-pro-14-gen10.nix
+++ hardware/tuxedo-infinitybook-pro-14-gen10.nix
@@ -70,9 +70,9 @@
     };
 
     tuxedo-drivers.enable = true;
-    #    tuxedo-rs = {
-    #      enable = true;
-    #      tailor-gui.enable = true;
-    #    };
+    tuxedo-rs = {
+      enable = true;
+      tailor-gui.enable = true;
+    };
   };
 }
