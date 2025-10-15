6cb1907e03cd92eb1fec5882948e171e97f35833
painerp <8081128+painerp@users.noreply.github.com>
2025-10-15 20:29:06 +0200

    Revert "chore(deps): update docker.io/freikin/dawarich docker tag to v0.33.1"
    This reverts commit f3983b2edec2b0326d03787cbceb98684e6acdb1.

--- variants/inf.nix
+++ variants/inf.nix
@@ -85,7 +85,7 @@ in
     };
     dawarich = {
       enable = true;
-      version = "0.33.1";
+      version = "0.33.0";
       env-file = secrets.inf-dawarich-env;
       postgres.env-file = secrets.inf-dawarich-pg-env;
     };
