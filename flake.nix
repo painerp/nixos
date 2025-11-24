645763142dd49b2dd1c8fc3c16ecef9b9573e150
painerp <8081128+painerp@users.noreply.github.com>
2025-11-24 22:43:28 +0100

    nixos: update state to 25.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "system config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     arion = {
@@ -21,7 +21,7 @@
     nix-deploy.url = "github:painerp/nix-deploy";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
