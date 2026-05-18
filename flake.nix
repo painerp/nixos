a366be421d5cf66388cd7f1fcb2306d28a62ad7c
painerp <8081128+painerp@users.noreply.github.com>
2026-05-18 12:30:47 +0200

    flake: improve declaration

--- flake.nix
+++ flake.nix
@@ -72,293 +72,130 @@
       desktop-insecure-packages = [
         "qtwebengine-5.15.19"
       ];
+
+      mkServer =
+        {
+          name,
+          system ? "x86_64-linux",
+          hardware,
+          pkgConfig ? { },
+        }:
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs system;
+          pkgs = (import nixpkgs) ({ inherit system; } // pkgConfig);
+          modules = server-modules ++ [
+            ./variants/${name}.nix
+            hardware
+          ];
+        };
+
+      mkDesktop =
+        { name, hardware }:
+        let
+          system = "x86_64-linux";
+          pkgs-unstable = (import nixpkgs-unstable) {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs secrets pkgs-unstable;
+          };
+          inherit system;
+          pkgs = (import nixpkgs) {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              permittedInsecurePackages = desktop-insecure-packages;
+            };
+            overlays = desktop-overlays;
+          };
+          modules = desktop-modules ++ [
+            ./variants/${name}.nix
+            hardware
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "bak";
+              home-manager.users.${name} = import ./variants/homes/default.nix;
+              home-manager.extraSpecialArgs = {
+                inherit inputs pkgs-unstable;
+              };
+            }
+          ];
+        };
     in
     {
       nixosConfigurations = {
-        fpi =
-          let
-            system = "aarch64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/fpi.nix
-              ./hardware/rpi.nix
-            ];
+        # Servers
+        fpi = mkServer {
+          name = "fpi";
+          system = "aarch64-linux";
+          hardware = ./hardware/rpi.nix;
+        };
+        sex = mkServer {
+          name = "sex";
+          hardware = ./hardware/vps.nix;
+        };
+        run = mkServer {
+          name = "run";
+          hardware = ./hardware/int-vps.nix;
+        };
+        log = mkServer {
+          name = "log";
+          hardware = ./hardware/int-vps.nix;
+        };
+        cit = mkServer {
+          name = "cit";
+          hardware = ./hardware/int-vps.nix;
+        };
+        inf = mkServer {
+          name = "inf";
+          hardware = ./hardware/int-vps.nix;
+        };
+        gra = mkServer {
+          name = "gra";
+          hardware = ./hardware/int-vps.nix;
+          pkgConfig = {
+            overlays = [ inputs.nvidia-patch.overlays.default ];
+            config.allowUnfree = true;
           };
+        };
+        gam = mkServer {
+          name = "gam";
+          hardware = ./hardware/int-vps.nix;
+        };
+        arr = mkServer {
+          name = "arr";
+          hardware = ./hardware/int-vps.nix;
+        };
+        jbx = mkServer {
+          name = "jbx";
+          hardware = ./hardware/thinkcentre-m715q.nix;
+          pkgConfig = {
+            config.allowUnfree = true;
+          };
+        };
 
-        sex =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/sex.nix
-              ./hardware/vps.nix
-            ];
-          };
-
-        run =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/run.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        log =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/log.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        cit =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/cit.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        inf =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/inf.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        gra =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) {
-              inherit system;
-              overlays = [ inputs.nvidia-patch.overlays.default ];
-              config.allowUnfree = true;
-            };
-            modules = server-modules ++ [
-              ./variants/gra.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        gam =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/gam.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        arr =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) { inherit system; };
-            modules = server-modules ++ [
-              ./variants/arr.nix
-              ./hardware/int-vps.nix
-            ];
-          };
-
-        kronos =
-          let
-            system = "x86_64-linux";
-            pkgs-unstable = (import nixpkgs-unstable) {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          in
-          nixpkgs.lib.nixosSystem {
-            specialArgs = {
-              inherit inputs secrets pkgs-unstable;
-            };
-            inherit system;
-            pkgs = (import nixpkgs) {
-              inherit system;
-              config = {
-                allowUnfree = true;
-                permittedInsecurePackages = desktop-insecure-packages;
-              };
-              overlays = desktop-overlays;
-            };
-            modules = desktop-modules ++ [
-              ./variants/kronos.nix
-              ./hardware/lenovo-legion-15arh05h.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.users.kronos = import ./variants/homes/default.nix;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs pkgs-unstable;
-                };
-              }
-            ];
-          };
-
-        demeter =
-          let
-            system = "x86_64-linux";
-            pkgs-unstable = (import nixpkgs-unstable) {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          in
-          nixpkgs.lib.nixosSystem {
-            specialArgs = {
-              inherit inputs secrets pkgs-unstable;
-            };
-            inherit system;
-            pkgs = (import nixpkgs) {
-              inherit system;
-              config = {
-                allowUnfree = true;
-                permittedInsecurePackages = desktop-insecure-packages;
-              };
-              overlays = desktop-overlays;
-            };
-            modules = desktop-modules ++ [
-              ./variants/demeter.nix
-              ./hardware/lenovo-legion-15arh05h.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.users.demeter = import ./variants/homes/default.nix;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs pkgs-unstable;
-                };
-              }
-            ];
-          };
-
-        dionysus =
-          let
-            system = "x86_64-linux";
-            pkgs-unstable = (import nixpkgs-unstable) {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          in
-          nixpkgs.lib.nixosSystem {
-            specialArgs = {
-              inherit inputs secrets pkgs-unstable;
-            };
-            inherit system;
-            pkgs = (import nixpkgs) {
-              inherit system;
-              config = {
-                allowUnfree = true;
-                permittedInsecurePackages = desktop-insecure-packages;
-              };
-              overlays = desktop-overlays;
-            };
-            modules = desktop-modules ++ [
-              ./variants/dionysus.nix
-              ./hardware/amd-5800x3d.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.users.dionysus = import ./variants/homes/default.nix;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs pkgs-unstable;
-                };
-              }
-            ];
-          };
-
-        jbx =
-          let
-            system = "x86_64-linux";
-          in
-          nixpkgs.lib.nixosSystem {
-            inherit specialArgs system;
-            pkgs = (import nixpkgs) {
-              inherit system;
-              config.allowUnfree = true;
-            };
-            modules = server-modules ++ [
-              ./variants/jbx.nix
-              ./hardware/thinkcentre-m715q.nix
-            ];
-          };
-
-        artemis =
-          let
-            system = "x86_64-linux";
-            pkgs-unstable = (import nixpkgs-unstable) {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          in
-          nixpkgs.lib.nixosSystem {
-            specialArgs = {
-              inherit inputs secrets pkgs-unstable;
-            };
-            inherit system;
-            pkgs = (import nixpkgs) {
-              inherit system;
-              config = {
-                allowUnfree = true;
-                permittedInsecurePackages = desktop-insecure-packages;
-              };
-              overlays = desktop-overlays;
-            };
-            modules = desktop-modules ++ [
-              ./variants/artemis.nix
-              ./hardware/tuxedo-infinitybook-pro-14-gen10.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "bak";
-                home-manager.users.artemis = import ./variants/homes/default.nix;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs pkgs-unstable;
-                };
-              }
-            ];
-          };
+        # Desktops
+        kronos = mkDesktop {
+          name = "kronos";
+          hardware = ./hardware/lenovo-legion-15arh05h.nix;
+        };
+        demeter = mkDesktop {
+          name = "demeter";
+          hardware = ./hardware/lenovo-legion-15arh05h.nix;
+        };
+        dionysus = mkDesktop {
+          name = "dionysus";
+          hardware = ./hardware/amd-5800x3d.nix;
+        };
+        artemis = mkDesktop {
+          name = "artemis";
+          hardware = ./hardware/tuxedo-infinitybook-pro-14-gen10.nix;
+        };
       };
     };
 }
